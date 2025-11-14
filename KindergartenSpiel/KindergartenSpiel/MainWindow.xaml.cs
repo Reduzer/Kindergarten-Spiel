bc463a7ac13535f088a6901e06dd26644b36300a
BjarneObst <143389462+BjarneObst@users.noreply.github.com>
2025-11-14 13:14:34 +0100

    Hintergrund angepasst, BAr beabreitet, funktion eweitert

--- KindergartenSpiel/KindergartenSpiel/MainWindow.xaml.cs
+++ KindergartenSpiel/KindergartenSpiel/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using KindergartenSpiel.Seiten;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,50 @@ namespace KindergartenSpiel
         public MainWindow()
         {
             InitializeComponent();
+            var fp = new Start();
+            NavFrame.Navigate(fp);
+        }
+
+        public void SetPage(Page nextPage)
+        {
+            NavFrame.Navigate(nextPage);
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+                this.WindowState = WindowState.Normal;
+            else
+                this.WindowState = WindowState.Maximized;
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+                ((Border)this.Content).CornerRadius = new CornerRadius(0);
+            else
+                ((Border)this.Content).CornerRadius = new CornerRadius(20);
+        }
+
+        private void Maximieren_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
